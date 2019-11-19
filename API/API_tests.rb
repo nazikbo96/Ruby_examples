fe6c9c361098feef57a421ab314892692ffca70a
Nazarii Volodymyrovych Yurkiv <nyurk@macbook7930.ddns.softservecom.com>
2019-11-19 16:19:27 +0200

    Resolve conversation

--- API/API_tests.rb
+++ API/API_tests.rb
@@ -10,7 +10,6 @@ let(:message_id) { send_message(service, user_id,test_message) }
   it 'verify sending mail' do
     expect(message_id.to_s.empty?).to be false
     expect(service.get_user_message(user_id, message_id ).to_s.empty?).to be false
-    #expect((service.get_user_message(user_id, message_id).snippet).include? File.read('body.txt')).to be true
-    expect((File.read('body.txt')).include? (service.get_user_message(user_id, message_id).snippet)).to be true
+    expect((service.get_user_message(user_id, message_id).snippet).include? File.read('body.txt')).to be true
   end
 end
